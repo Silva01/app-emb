8e8ccf0334ba63ec2c1cb70b19d3e8237f6d1e7f
pedroventura88 <pedro.asventura@gmail.com>
2020-05-02 17:28:40 -0300

    [Pedro Andrade]: Commit Inicial do projeto. Criação do pom e suas dependências.

---
+++ src/main/java/app/embadm/EmbAdmApplication.java
@@ -0,0 +1,13 @@
+package app.embadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbAdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbAdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/embadm/EmbAdmApplicationTests.java
@@ -0,0 +1,13 @@
+package app.embadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbAdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
