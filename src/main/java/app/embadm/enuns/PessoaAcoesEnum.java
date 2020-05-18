334388f00d20c9d848183624f4dbc3610736ac18
pedroventura88 <pedro.asventura@gmail.com>
2020-05-18 19:55:47 -0300

    [Pedro Andrade]: Criação do ENUM PessoaAcoesEnum

---
+++ src/main/java/app/embadm/enuns/PessoaAcoesEnum.java
@@ -0,0 +1,21 @@
+package app.embadm.enuns;
+
+import app.embadm.Acao;
+
+public enum PessoaAcoesEnum implements Acao {
+
+    REDIRECIONAR_PESSOA("redirect:/pessoas/listar-pessoas"),
+    LISTAR_PESSOA_ENDPOINT("pessoa/listar-pessoas"),
+    INCLUIR_PESSOA("pessoa/inserir-pessoa");
+
+    private String acao;
+
+    @Override
+    public String getAcao() {
+        return null;
+    }
+
+    PessoaAcoesEnum(String acao) {
+        this.acao = acao;
+    }
+}
