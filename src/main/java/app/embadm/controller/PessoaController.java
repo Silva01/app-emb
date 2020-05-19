6c3e02f1dbd6a12e84efeb084f6724ea13ed4871
pedroventura88 <pedro.asventura@gmail.com>
2020-05-19 11:41:18 -0300

    [Pedro Andrade]: Refatorando PessoaController, e inserindo Perfil como relação.

--- src/main/java/app/embadm/controller/PessoaController.java
+++ src/main/java/app/embadm/controller/PessoaController.java
@@ -1,6 +1,8 @@
 package app.embadm.controller;
 
 import app.embadm.entity.PessoaEntidade;
+import app.embadm.enuns.PessoaAcoesEnum;
+import app.embadm.service.PerfilService;
 import app.embadm.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,37 +17,42 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
+    @Autowired
+    private PerfilService perfilService;
+
     @GetMapping("inserir-pessoa")
     public String inserirPessoa (Model model) {
-        PessoaEntidade pessoaEntidade = new PessoaEntidade();
-        model.addAttribute("pessoa", pessoaEntidade);
-        return "pessoa/inserir-pessoa";
+        return pessoaService.obterPessoaPorId(0)
+                .adicionarAtributoDaResposta("perfis", perfilService.listarTodosOsPerfis())
+                .uploadAtributosModelo(model)
+                .redirecionar(PessoaAcoesEnum.INCLUIR_PESSOA);
     }
 
     @RequestMapping(path = "salvar-pessoa", method = RequestMethod.POST)
     public String salvar(PessoaEntidade pessoaEntidade) {
         pessoaEntidade.setAtivo(true);
-        pessoaService.salvarPessoa(pessoaEntidade);
-        return "redirect:/pessoas/listar-pessoas";
+        return pessoaService.salvarPessoa(pessoaEntidade).redirecionar(PessoaAcoesEnum.REDIRECIONAR_PESSOA);
     }
 
     @GetMapping("listar-pessoas")
     public String listarPessoas (Model model) {
-        model.addAttribute("listarPessoas", pessoaService.listarTodasPessoas());
-        return "pessoa/listar-pessoas";
+        return pessoaService.listarTodasPessoas()
+                .uploadAtributosModelo(model)
+                .redirecionar(PessoaAcoesEnum.LISTAR_PESSOA_ENDPOINT);
     }
 
     @RequestMapping(path = "/deletar-pessoa/{cpf}")
     public String deletarPessoaPorCpf (@PathVariable("cpf") String cpf, Model model) {
-        pessoaService.deletarPessoaPorCpf(cpf);
-        model.addAttribute("listarPessoas", pessoaService.listarTodasPessoas());
-        return "redirect:/pessoas/listar-pessoas";
+        return pessoaService.deletarPessoaPorCpf(cpf).listarTodasPessoas().uploadAtributosModelo(model)
+                .redirecionar(PessoaAcoesEnum.REDIRECIONAR_PESSOA);
     }
 
     @GetMapping("atualizar-pessoa/{cpf}")
     public String atualizarPessoaPorCpf (@PathVariable("cpf") String cpf, Model model) {
-        model.addAttribute("pessoa", pessoaService.obterPessoaPorCpf(cpf));
-        return "pessoa/inserir-pessoa";
+        return pessoaService.obterPessoaPorCpf(cpf)
+            .adicionarAtributoDaResposta("perfis", perfilService.listarTodosOsPerfis())
+            .uploadAtributosModelo(model)
+            .redirecionar(PessoaAcoesEnum.INCLUIR_PESSOA);
     }
 
 }
