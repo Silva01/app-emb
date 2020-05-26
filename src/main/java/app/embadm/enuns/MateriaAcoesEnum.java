a5bf38c8caad2df2ee144f60d9520c66bd7cb8c1
pedroventura88 <pedro.asventura@gmail.com>
2020-05-26 20:10:56 -0300

    [Pedro Andrade]: Enum ações de Materia adicionado.

---
+++ src/main/java/app/embadm/enuns/MateriaAcoesEnum.java
@@ -0,0 +1,22 @@
+package app.embadm.enuns;
+
+import app.embadm.Acao;
+
+public enum MateriaAcoesEnum implements Acao {
+
+    REDIRECIONAR_MATERIA("redirect:/materias/listar-materias"),
+    LISTAR_MATERIA_ENDPOINT("materias/listar-materias"),
+    INCLUIR_MATERIA("materia/inserir-materia");
+
+    private String acao;
+
+    @Override
+    public String getAcao() {
+        return acao;
+    }
+
+    MateriaAcoesEnum(String acao) {
+        this.acao = acao;
+    }
+
+}
